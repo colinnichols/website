28a7dda340cbff6687cd6d25dc84622a85cba265
Colin <colinnic@gmail.com>
2025-03-21 11:26:52 -0600

    Added compression

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { compression } from "vite-plugin-compression2";
 
 export default defineConfig({
-  plugins: [tailwindcss(), TanStackRouterVite(), react()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    tailwindcss(),
+    compression({
+      include: [/\.(html|js|css)$/],
+    }),
+  ],
 });
