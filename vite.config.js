76e6bdec9d8dc65d8e9d2b87e0fc99855e634758
Colin <colinnic@gmail.com>
2025-03-21 21:32:26 -0600

    Cleanup removing compression from config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { compression } from "vite-plugin-compression2";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    TanStackRouterVite(),
-    tailwindcss(),
-    compression({
-      include: [/\.(html|js|css)$/],
-    }),
-  ],
+  plugins: [react(), TanStackRouterVite(), tailwindcss()],
 });
