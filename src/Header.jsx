2a82c9d1e9afd5569d033fdefd6362cf3b6fd952
Colin <colinnic@gmail.com>
2025-03-24 08:47:12 -0600

    Updated navigation menu markup and styles

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,30 +4,30 @@ export default function Header() {
   return (
     <header className="header">
       <div className="flex mx-auto px-[2rem] max-w-5xl">
-        <div className="grow">
-          <Link className="site-title" to="/">
+        <div className="grow content-center">
+          <Link className="logo" to="/">
             Colin Nichols
           </Link>
         </div>
-        <nav className="site-nav">
+        <nav className="navigation">
           <input
             type="checkbox"
-            id="nav-trigger"
-            className="nav-trigger sr-only"
+            id="navigation-state"
+            className="navigation-state"
+            onChange=""
           />
-          <label htmlFor="nav-trigger" tabIndex="0">
-            <span className="menu-icon">
-              <span className="sr-only">Menu Toggle</span>
-              <img className="fa" src="/img/bars.svg" aria-hidden="true" />
-            </span>
+          <label htmlFor="navigation-state" className="navigation-trigger">
+            <span className="sr-only">Menu Toggle</span>
+            <img
+              className="fa"
+              src="/img/bars.svg"
+              title="Menu Toggle"
+              aria-hidden="true"
+            />
           </label>
-          <div className="trigger">
-            <Link className="page-link" to="/about">
-              About
-            </Link>
-            <Link className="page-link" to="/resume">
-              Resume
-            </Link>
+          <div className="navigation-menu">
+            <Link to="/">About</Link>
+            <Link to="/resume">Resume</Link>
           </div>
         </nav>
       </div>
